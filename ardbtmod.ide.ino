70319272f167453107b646893d28528a2b1da781
Sudip Roy <roy.star97@gmail.com>
2017-06-03 02:58:25 +0530

    Add files via upload

---
+++ ardbtmod.ide.ino
@@ -0,0 +1,29 @@
+//Code for command mode control of arduino from phone by Sudip_roy
+
+
+int led = 13;
+void setup()
+{
+  Serial.begin(9600);
+  
+  pinMode(led,OUTPUT);
+  digitalWrite(led,LOW);
+}
+void loop()
+{
+ if(Serial.available())
+ {
+   String value = Serial.readStringUntil('\n');
+   Serial.println(value);
+   if(value == "on")
+   {
+     digitalWrite(led,HIGH);
+   }
+   else if(value == "off")
+   {
+     digitalWrite(led,LOW);
+   }
+  
+ }
+}
+
